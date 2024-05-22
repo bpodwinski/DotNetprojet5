871671163a329026870b6d0ac85bc3f779bfde38
Benoit Podwinski <me@benoitpodwinski.com>
2024-05-22 22:26:59 +0200

    Add validation for Year, Vin, Brand, Model, and TrimLevel fields in Vehicle entity

--- Express Voitures/Models/Entities/Vehicle.cs
+++ Express Voitures/Models/Entities/Vehicle.cs
@@ -15,17 +15,22 @@ namespace Express_Voitures.Models.Entities
         [SwaggerSchema(ReadOnly = true)]
         public DateTime CreateDate { get; set; }
 
+        [RegularExpression(@"^[A-HJ-NPR-Z0-9]{17}$", ErrorMessage = "Please enter a valid 17-character VIN")]
         public string Vin { get; set; }
 
         [Required]
+        [Range(1886, 9999, ErrorMessage = "Please enter a valid year between 1886 and 9999")]
         public string Year { get; set; }
 
         [Required]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Brand name must be between 1 and 50 characters")]
         public string Brand { get; set; }
 
         [Required]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Model name must be between 1 and 50 characters")]
         public string Model { get; set; }
 
+        [StringLength(50, ErrorMessage = "Trim Level name cannot exceed 50 characters")]
         public string TrimLevel { get; set; }
     }
 }
